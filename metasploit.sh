a04104f1c895166bb686eb3106be8b1477531987
mangakrack <51804124+mangakrack@users.noreply.github.com>
2019-06-15 12:20:12 -0300

    metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,25 +1,26 @@
-#!/data/data/com.termux/files/usr/bin/bash                    
+#!/data/data/com.termux/files/usr/bin/bash
+
 #instalando metasploit
 
 pkg update
 
 pkg upgrade
+pkg instal figlet
 
 pkg install unstable-repo
 
 pkg install metasploit
-
-#limpiando
-clear
-#bienvenida
-echo "      #######################                                 |hola metasploit a sido|
-      |instalado             |
-      |para arrancar escribe |
-      |msfconsole            |
-       #######################"
-#arrancando metasploit
-pg_ctl -D /data/data/com.termux/files/usr/var/lib/postgresql -
-l logfile start
-msfconsole
 #limpieza
 clear
+
+#bienvenida
+setterm -foreground cyan
+
+figlet  metasploit instalado
+
+      setterm -foreground white
+#arrancando metasploit
+pg_ctl -D /data/data/com.termux/files/usr/var/lib/postgresql -l logfile start
+msfconsole
+
+ 
